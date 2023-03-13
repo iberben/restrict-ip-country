1e3d93efaf6586e2619204d7800e2447fb4633a5
ron4fun <ron2tele@gmail.com>
2023-03-13 18:32:53 +0100

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -5,7 +5,6 @@ import warnings
 
 import django, os
 from pathlib import Path
-from django.urls import reverse_lazy
 from django.conf import settings
 from django.test.utils import get_runner
 
@@ -39,7 +38,7 @@ DEFAULT_SETTINGS = dict(
     # GEOIP_CITY='GeoLite2-City.mmdb',
     RESTRICT_IP_CACHE_TIMEOUT=0,
     RESTRICT_IP_TEMPLATE_PATH = 'errors/403.html',
-    RESTRICT_IP_TEMPLATE_CONTEXT_BACKEND='restrict_ip_country.helpers.build_template_context'
+    RESTRICT_IP_TEMPLATE_CONTEXT_BACKEND='restrict_ip_country.utils.build_template_context'
 )
 
 
