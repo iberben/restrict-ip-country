f5479cc4c235ca24810799ec272289f68bdceac2
ron4fun <ron2tele@gmail.com>
2023-03-15 05:17:33 +0100

    Update mixin.py

--- restrict_ip_country/mixin.py
+++ restrict_ip_country/mixin.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class TimeStampMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, null=True, editable=False)
 
     class Meta:
         abstract = True       
